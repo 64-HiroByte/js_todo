93fb878bec470f4b6aa0c5a8e5f80f6b4e12176a
64-HiroByte <higher.ground.rc@gmail.com>
2025-08-19 23:33:27 +0900

    js-primerのサンプルコードをコピーした

--- index.js
+++ index.js
@@ -1,66 +1,118 @@
-// import { App } from "./src/App.js";
-// const app = new App();
+import { App } from "./src/App.js";
 
-const form = document.getElementById('js-form');
-const formInput = document.getElementById('js-form-input');
-const ul = document.getElementById('ul');
+const formElement = document.querySelector("#js-form");
+const formInputElement = document.querySelector("#js-form-input");
+const todoCountElement = document.querySelector("#js-todo-count");
+const todoListContainerElement = document.querySelector("#js-todo-list");
 
-const todos = JSON.parse(localStorage.getItem('todos'));
-
-if (todos) {
-  todos.forEach(todo => {
-    add(todo);
-  });
-}
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  add();
+const app = new App({
+    formElement,
+    formInputElement,
+    todoCountElement,
+    todoListContainerElement
+});
+window.addEventListener("load", () => {
+    app.mount();
+});
+window.addEventListener("unload", () => {
+    app.unmount();
 });
 
-function add(todo) {
-  let todoText = formInput.value;
-  if (todo) {
-    todoText = todo.text;
-  }
+// const form = document.getElementById('js-form');
+// const formInput = document.getElementById('js-form-input');
+// const ul = document.getElementById('ul');
 
-  if (todoText) {
-    const li = document.createElement('li');
-    li.innerText = todoText;
-    li.classList.add('list-group-item');
+// const todos = JSON.parse(localStorage.getItem('todos'));
 
-    if (todo && todo.completed) {
-      li.classList.add('text-decoration-line-through');
-    }
+// if (todos) {
+//   todos.forEach(todo => {
+//     add(todo);
+//   });
+// }
 
-    // 削除処理（右クリック）
-    li.addEventListener('contextmenu', function(event) {
-      event.preventDefault();
-      li.remove();
-      saveData();
-    });
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   add();
+// });
 
-    // 完了処理（左クリック）
-    li.addEventListener('click', function () {
-      li.classList.toggle('text-decoration-line-through');
-      saveData();
-    });
+// function add(todo) {
+//   let todoText = formInput.value;
+//   if (todo) {
+//     todoText = todo.text;
+//   }
 
-    ul.appendChild(li);
-    formInput.value = '';
-    saveData();
-  }
-}
+//   if (todoText) {
+//     // li要素
+//     const li = document.createElement('li');
+//     // li.innerText = todoText;
+//     li.classList.add('list-group-item', 'form-check');
 
-function saveData() {
-  const lists = document.querySelectorAll('li');
-  let todos = [];
-  lists.forEach(list => {
-    let todo = {
-      text:list.innerText,
-      completed: list.classList.contains('text-decoration-line-through')
-    }
-    todos.push(todo);
-  });
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
+//     // チェックボックス
+//     const checkbox = document.createElement('input');
+//     checkbox.classList.add('me-1');
+//     checkbox.type = 'checkbox';
+
+//     // Todoタスクの内容
+//     const label = document.createElement('label');
+//     // label.classList = 'form-check-label';
+//     label.textContent = todoText;
+
+//     // 編集ボタン
+//     const editButton = document.createElement('button');
+//     editButton.classList = 'btn btn-primary'
+//     editButton.textContent = '編集';
+
+//     // 削除ボタン
+//     const deleteButton = document.createElement('button');
+//     deleteButton.classList = 'btn btn-danger'
+//     deleteButton.textContent = '削除'
+
+//     deleteButton.addEventListener('click', () => {
+//       if (confirm('このタスクを削除しますか？')) {
+//         li.remove()
+//       }
+//     });
+
+//     // liに追加
+//     li.appendChild(checkbox);
+//     li.appendChild(label);
+//     li.appendChild(editButton);
+//     li.appendChild(deleteButton);
+
+//     if (todo && todo.completed) {
+//       li.classList.add('text-decoration-line-through');
+//     }
+
+    // // 削除処理（右クリック）
+    // li.addEventListener('contextmenu', function(event) {
+    //   event.preventDefault();
+    //   li.remove();
+    //   saveData();
+    // });
+
+    // // 完了処理（左クリック）
+    // li.addEventListener('click', function () {
+    //   li.classList.toggle('text-decoration-line-through');
+    //   saveData();
+    // });
+
+//     ul.appendChild(li);
+//     formInput.value = '';
+//     saveData();
+//   }
+// }
+
+// function saveData() {
+//   const lists = document.querySelectorAll('li');
+//   let todos = [];
+//   lists.forEach(list => {
+//     const label = document.querySelector('label');
+//     let todo = {
+//       text:label.innerText,
+//       // text:list.innerText,
+//       completed: list.classList.contains('text-decoration-line-through')
+//     }
+//     todos.push(todo);
+//   });
+//   localStorage.setItem('todos', JSON.stringify(todos));
+// }
