82a73315827de3bd93b0bb813dd6afe95d144251
64-HiroByte <higher.ground.rc@gmail.com>
2025-08-26 01:19:03 +0900

    TodoItemView.jsのリファクタリングを行った

--- src/view/TodoItemView.js
+++ src/view/TodoItemView.js
@@ -10,19 +10,35 @@ export class TodoItemView {
    * @returns {Element}
    */
   createElement(todoItem, { onUpdateTodo, onDeleteTodo, onEditTodo }) {
-    const todoItemElement = todoItem.completed
-      ? element`
-        <li class="list-group-item"><input type="checkbox" class="checkbox" checked>
-          <s>${todoItem.title}</s>
-          <button class="btn btn-success edit">編集</button>
-          <button class="btn btn-danger delete">削除</button>
-        </li>`
-      : element`
-        <li class="list-group-item"><input type="checkbox" class="checkbox">
-          ${todoItem.title}
-          <button class="btn btn-success edit">編集</button>
-          <button class="btn btn-danger delete">削除</button>
-        </li>`;
+    // const todoItemElement = todoItem.completed
+    //   ? element`
+    //     <li class="list-group-item"><input type="checkbox" class="checkbox" checked>
+    //       <s>${todoItem.title}</s>
+    //       <button class="btn btn-success edit">編集</button>
+    //       <button class="btn btn-danger delete">削除</button>
+    //     </li>`
+    //   : element`
+    //     <li class="list-group-item"><input type="checkbox" class="checkbox">
+    //       ${todoItem.title}
+    //       <button class="btn btn-success edit">編集</button>
+    //       <button class="btn btn-danger delete">削除</button>
+    //     </li>`;
+    const todoItemElement = element`
+      <li class="list-group-item">
+        <input type="checkbox" class="checkbox">
+        <span class="title">${todoItem.title}</span>
+        <button class="btn btn-success edit">編集</button>
+        <button class="btn btn-danger delete">削除</button>
+      </li>`;
+
+    // completed の場合だけUIを調整
+    if (todoItem.completed) {
+      const checkbox = todoItemElement.querySelector(".checkbox");
+      const titleSpan = todoItemElement.querySelector(".title");
+      checkbox.checked = true;
+      titleSpan.innerHTML = `<s>${todoItem.title}</s>`;
+    }
+
 
     // チェックボックス
     const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
@@ -40,28 +56,20 @@ export class TodoItemView {
       const modalElement = document.getElementById('deleteConfirmModal');
       const confirmButton = document.getElementById('confirmDeleteBtn');
       const bsModal = new bootstrap.Modal(modalElement);
-      bsModal.show()
+      bsModal.show();
 
       const handler = () => {
         onDeleteTodo({ id: todoItem.id });
-        bsModal.hide()
+        bsModal.hide();
         confirmButton.removeEventListener('click', handler);
       };
       confirmButton.addEventListener('click', handler);
-
-      // if (confirm('本当に削除してよろしいですか？')) {
-        
-      //   // コールバック関数に変更
-      //   onDeleteTodo({
-      //     id: todoItem.id
-      //   });
-      // }
     });
 
     // 編集
     const editButtonElement = todoItemElement.querySelector('.edit');
     editButtonElement.addEventListener('click', () => {
-      // 編集モードUIに変更
+      // 編集モードUI
       const editElement = document.createElement('div');
       editElement.className = 'd-flex gap-2'
       const inputElement = document.createElement('input');
@@ -78,14 +86,12 @@ export class TodoItemView {
       todoItemElement.innerHTML = ''
       // 編集モードUIを挿入
       todoItemElement.appendChild(editElement);
-      // todoItemElement.appendChild(inputElement);
-      // todoItemElement.appendChild(saveButtonElement);
 
       saveButtonElement.addEventListener('click', () => {
         const newTitle = inputElement.value;
         onEditTodo({ id: todoItem.id, title: newTitle});
-      })
-    })
+      });
+    });
 
     // 作成したTodoアイテムのHTML要素を返す
     return todoItemElement;
